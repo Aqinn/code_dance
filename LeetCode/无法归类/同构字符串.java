b891745a9ab55576afa386663f966acc5330a30e
Aqinn <zzqzzf@outlook.com>
2021-04-03 13:06:38 +0800

    2021.04.03 1题

---
+++ LeetCode/无法归类/同构字符串.java
@@ -0,0 +1,34 @@
+package LeetCode.无法归类;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author Aqinn
+ * @Date 2021/4/3 12:51 下午
+ */
+public class 同构字符串 {
+
+    /**
+     * 题目描述:
+     * 给定两个字符串s和t，判断它们是否是同构的。
+     * 如果s中的字符可以按某种映射关系替换得到t，那么这两个字符串是同构的。
+     * 每个出现的字符都应当映射到另一个字符，同时不改变字符的顺序。不同字符不能映射到同一个字符上，相同字符只能映射到同一个字符上，字符可以映射到自己本身。
+     */
+
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> s2t = new HashMap<Character, Character>();
+        Map<Character, Character> t2s = new HashMap<Character, Character>();
+        int len = s.length();
+        for (int i = 0; i < len; ++i) {
+            char x = s.charAt(i), y = t.charAt(i);
+            if ((s2t.containsKey(x) && s2t.get(x) != y) || (t2s.containsKey(y) && t2s.get(y) != x)) {
+                return false;
+            }
+            s2t.put(x, y);
+            t2s.put(y, x);
+        }
+        return true;
+    }
+
+}
