1228beffbc2ceabcd322a72f50be018c8e9337cf
Aqinn <zzqzzf@outlook.com>
2021-04-28 11:59:22 +0800

    2021.04.28 1题

---
+++ LeetCode/无法归类/存在重复元素2.java
@@ -0,0 +1,35 @@
+package LeetCode.无法归类;
+
+import java.util.*;
+
+/**
+ * @Author Aqinn
+ * @Date 2021/4/28 11:55 上午
+ */
+public class 存在重复元素2 {
+
+    /**
+     * 题目描述:
+     * 给定一个整数数组和一个整数k，判断数组中是否存在两个不同的索引i和j，使得nums [i] = nums [j]，并且 i 和 j的差的 绝对值 至多为 k。
+     * <p>
+     * 输入: nums = [1,2,3,1], k = 3
+     * 输出: true
+     * <p>
+     * 输入: nums = [1,2,3,1,2,3], k = 2
+     * 输出: false
+     **/
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if (i - k >= 0)
+                set.remove(nums[i - k]);
+        }
+        return false;
+    }
+
+}
