1e45f2484fb42b6c51aaa09700ebdc9d008542aa
Aqinn <zzqzzf@outlook.com>
2021-05-13 12:00:42 +0800

    2021.05.13 1题

---
+++ LeetCode/链表/回文链表.kt
@@ -0,0 +1,54 @@
+package LeetCode.链表
+
+/**
+ * @Author Aqinn
+ * @Date 2021/5/13 10:45 上午
+ */
+class 回文链表 {
+
+    /**
+     * 题目描述:
+     * 请判断一个链表是否为回文链表。
+     */
+
+    fun isPalindrome(head: ListNode?): Boolean {
+        if (head == null) {
+            return false
+        }
+        // 快满指针找出中间节点
+        var slow = head
+        var fast = head
+        while (fast!!.next != null && fast.next!!.next != null) {
+            slow = slow!!.next!!
+            fast = fast.next!!.next!!
+        }
+        // 翻转 slow 后面的链表
+        var cur = slow?.next
+        var pre: ListNode? = null
+        while (cur != null) {
+            val next = cur.next
+            cur.next = pre
+            pre = cur
+            cur = next
+        }
+        if (slow != null) {
+            slow.next = pre
+        }
+        // 判断
+        var first = head
+        var second = slow!!.next
+        while (second != null) {
+            if (first!!.`val` != second.`val`) {
+                return false
+            }
+            first = first.next
+            second = second.next
+        }
+        return true
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+
+}
