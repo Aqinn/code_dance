c578e4af6881b28a8307c25ddaef21c78a89fef4
Aqinn <zzqzzf@outlook.com>
2021-05-21 16:28:29 +0800

    2021.05.21 1题

---
+++ LeetCode/动态规划/整数拆分.kt
@@ -0,0 +1,28 @@
+package LeetCode.动态规划
+
+import kotlin.math.max
+
+/**
+ * @Author Aqinn
+ * @Date 2021/5/21 4:13 下午
+ */
+class 整数拆分 {
+
+    /**
+     * 题目描述:
+     * 给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
+     */
+
+    fun integerBreak(n: Int): Int {
+        val dp = IntArray(n + 1)
+        dp[0] = 0
+        dp[1] = 0
+        for (i in 2..n) {
+            for (j in (i - 1) downTo 1) {
+                dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]))
+            }
+        }
+        return dp[n]
+    }
+
+}
